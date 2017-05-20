0eacf6fc93a511102d0e9439d9e350c043e0906b
Jakub Woźniak <j.wozniak@tenesys.pl>
2017-05-20 21:06:50 +0200

    some fixes

--- awslist.go
+++ awslist.go
@@ -19,22 +19,27 @@ func InstancesList(svc *ec2.EC2) (instances []*ec2.Instance) {
 
     for _, r := range resp.Reservations {
         for _, i := range r.Instances {
-            instances = append(instances, i) //TODO: remove reallactions
+            instances = append(instances, i) //TODO: remove reallocations
         }
     }
 
     return instances
 }
 
-func GetTagValue(ts []*ec2.Tag, name string) string {
+func Tags2Map(ts []*ec2.Tag) (tags map[string]string) {
+    tags = map[string]string{}
     for _, t := range ts {
-        if *t.Key == name {
-            return *t.Value
-        }
+        tags[*t.Key] = *t.Value
     }
+    return tags
+}
 
+func GetVal(ts map[string]string, n string) string {
+    if v, ok := ts[n]; ok == true {
+        return v
+    }
     return ""
-} 
+}
 
 func GenerateTable(data []*ec2.Instance) *tablewriter.Table {
     table := tablewriter.NewWriter(os.Stdout)
@@ -45,7 +50,8 @@ func GenerateTable(data []*ec2.Instance) *tablewriter.Table {
                              "Availability Zone"})
 
     for _, i := range data {
-        table.Append([]string{GetTagValue(i.Tags, "Name"), 
+        tags := Tags2Map(i.Tags)
+        table.Append([]string{GetVal(tags, "Name"), 
                               *i.InstanceType, 
                               *i.State.Name, 
                               *i.PrivateIpAddress, 
